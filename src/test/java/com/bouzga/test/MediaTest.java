ea627efc27253d286fc0bb6db4347d2363ceb25f
MountassirBOUZGA <MountassirBOUZGA@DESKTOP-O4BMO5M.lille.fr.ibm.com>
2018-12-24 16:23:39 +0100

    critere mongodb

--- src/test/java/com/bouzga/test/MediaTest.java
+++ src/test/java/com/bouzga/test/MediaTest.java
@@ -1,37 +1,68 @@
 package com.bouzga.test;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.bouzga.TestMongoDbApplicationTests;
+import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 
 @RunWith(SpringRunner.class)
-public class MediaTest extends TestMongoDbApplicationTests{
+public class MediaTest extends TestMongoDbApplicationTests {
 
 	private MongoClient mongoClient;
 	private MongoDatabase database;
 	private MongoCollection<Document> mediaCollection;
+
 	@Before
 	public void setup() {
 		this.mongoClient = new MongoClient("localhost", 27017);
 		this.database = mongoClient.getDatabase("my_db");
+		this.mediaCollection = database.getCollection("media");
 	}
-	
+
 	@Test
 	public void testMediaAll() {
 		mediaCollection = database.getCollection("media");
 		MongoCursor<Document> cursorMedia = mediaCollection.find().iterator();
-		 
+
 		while (cursorMedia.hasNext()) {
-		    System.out.println(cursorMedia.next());
+			System.out.println(cursorMedia.next());
 		}
+	}
+
+	@Test
+	public void testInsertMedia() {
+		Document media = new Document();
+		List<BasicDBObject> array = new ArrayList<BasicDBObject>();
+		array.add(new BasicDBObject("0", 12.3D));
+		array.add(new BasicDBObject("1", 32.1D));
+		media.put("city", "OUJDA");
+		media.put("loc", array);
+		media.put("pop", 1987);
+		media.put("state", "MA");
+
+		mediaCollection.insertOne(media);
+	}
+
+	@Test
+	public void testFindMediaById() {
+		Bson critere = Filters.eq("_id", "99950");
+		Document media = mediaCollection.find(critere).first();
+		assertEquals(media.get("city"), "KETCHIKAN");
 	}
 
 	public MongoClient getMongoClient() {
--- src/test/java/com/bouzga/test/PaysTest.java
+++ src/test/java/com/bouzga/test/PaysTest.java
@@ -1,8 +1,11 @@
 package com.bouzga.test;
 
+import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,6 +16,10 @@ import com.bouzga.TestMongoDbApplicationTests;
 import com.bouzga.entity.Pays;
 import com.bouzga.entity.Ville;
 import com.bouzga.repository.PaysRepository;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 
 @RunWith(SpringRunner.class)
 public class PaysTest extends TestMongoDbApplicationTests{
@@ -20,14 +27,18 @@ public class PaysTest extends TestMongoDbApplicationTests{
 	@Autowired
 	private PaysRepository paysRepo;
 	
+	private MongoClient mongoClient;
+	private MongoDatabase database;
+	private MongoCollection<Document> paysCollection;
+	
 	@Before
 	public void setup() {
+		this.mongoClient = new MongoClient("localhost", 27017);
+		this.database = mongoClient.getDatabase("my_db");
+		this.paysCollection = database.getCollection("pays");
+		
 		System.out.println("--- CLEAR PAYS ---");
 		paysRepo.deleteAll();
-	}
-	
-	@Test
-	public void savePays() {
 		System.out.println("--- SAVE PAYS ---");
 		Ville oujda = new Ville("1","Oujda", 500000, "Est");
 		Ville tanger = new Ville("2","Tanger", 1000000, "Nord");
@@ -37,7 +48,43 @@ public class PaysTest extends TestMongoDbApplicationTests{
 		villes.add(oujda);villes.add(tanger);villes.add(rabat);villes.add(agadir);
 		
 		Pays maroc = new Pays("1", "Maroc", villes);
-		Pays paysSaved = paysRepo.save(maroc);
-		System.out.println(paysSaved);
+		paysRepo.save(maroc);
 	}
+	
+	@Test
+	public void savePays() {
+
+	}
+
+	@Test
+	public void getPaysParCritere() {
+		Bson critere = Filters.and(Filters.eq("_id","1"),Filters.eq("nom", "Maroc"));
+		Document pays = paysCollection.find(critere).first();
+		assertEquals(pays.get("nom"), "Maroc");
+	}
+	
+	public MongoClient getMongoClient() {
+		return mongoClient;
+	}
+
+	public void setMongoClient(MongoClient mongoClient) {
+		this.mongoClient = mongoClient;
+	}
+
+	public MongoDatabase getDatabase() {
+		return database;
+	}
+
+	public void setDatabase(MongoDatabase database) {
+		this.database = database;
+	}
+
+	public MongoCollection<Document> getPaysCollection() {
+		return paysCollection;
+	}
+
+	public void setPaysCollection(MongoCollection<Document> paysCollection) {
+		this.paysCollection = paysCollection;
+	}
+	
 }
